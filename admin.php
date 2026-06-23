0cb3a2389c12dbf18118824be5ccdda18610fe27
linksprofile-cmd <linksprofile20@gmail.com>
2026-06-24 00:41:44 +0100

    admin.php

--- admin.php
+++ admin.php
@@ -1,8 +1,19 @@
 <?php
 
-include "db.php";
+$url = "https://ptshthtnjcbngiceyjzc.supabase.co/rest/v1/submissions?select=*&order=id.desc";
 
-$result = $conn->query("SELECT * FROM submissions ORDER BY id DESC");
+$ch = curl_init($url);
+
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($ch, CURLOPT_HTTPHEADER, array(
+    "apikey: eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InB0c2h0aHRuamNibmdpY2V5anpjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODIyNDc2NTYsImV4cCI6MjA5NzgyMzY1Nn0.sejLXlM95cllcRuFcG-18P9uVFTMSnL70svBZ1L0kag",
+    "Authorization: Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InB0c2h0aHRuamNibmdpY2V5anpjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODIyNDc2NTYsImV4cCI6MjA5NzgyMzY1Nn0.sejLXlM95cllcRuFcG-18P9uVFTMSnL70svBZ1L0kag"
+));
+
+$response = curl_exec($ch);
+curl_close($ch);
+
+$data = json_decode($response, true);
 
 ?>
 
