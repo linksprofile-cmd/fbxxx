f8b3f8ea0e00595e66d26925623d64d3f7c65c7a
linksprofile-cmd <linksprofile20@gmail.com>
2026-06-24 00:51:51 +0100

    db.php

--- db.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-$conn = new mysqli("yrvj8stc5f3zudb.pxxldb.pxxl.pro", "pxxluser_19ef4e632737e56", "e6cbx5Kv8laitR&_t7LLoTEAn4JIWFVq", "pxxluser_19ef4e632737e56", "33531");
-
-if ($conn->connect_error) {
-    die("Database connection failed");
-}
-
-?>
